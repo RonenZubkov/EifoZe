4be51d3d5062f7f39ccf43dbdf1eaf3fb4d7f329
zafrirdotan <zafrir.dotan@gmail.com>
2016-07-26 16:59:15 +0300

    fixed merge

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -3,21 +3,6 @@ import {AppComponent} from './app.component';
 import {MonsterListComponent} from './monster/monster-list.component';
 import {MonsterComponent} from './monster/monster.component';
 import {MonsterEditComponent} from './monster/monster-edit.component';
-<<<<<<< HEAD
-// import {ChatRoomComponent} from './chat/chat-room.component';
-import { RouterConfig, ROUTER_DIRECTIVES, provideRouter } from '@angular/router';
-import {MapComponent} from "./shared/map-component/map.component";
-
-
-
-const routes: RouterConfig = [
-  { path: '', component: AppComponent },
-  { path: 'map', component: MapComponent },
-  { path: 'monster', component: MonsterListComponent },
-  { path: 'monster/edit', component: MonsterEditComponent },
-  { path: 'monster/edit/:id', component: MonsterEditComponent },
-  { path: 'monster/:id/:name', component: MonsterComponent }
-=======
 
 import {LayerListComponent} from './layer/layer-list.component';
 import {LayerComponent} from './layer/layer.component';
@@ -35,7 +20,6 @@ const routes: RouterConfig = [
   { path: 'layer/edit', component: LayerEditComponent },
   { path: 'layer/edit/:id', component: LayerEditComponent },
   { path: 'layer/:id/:name', component: LayerEditComponent },
->>>>>>> origin/master
   // { path: 'chat', component: ChatRoomComponent }
 
 ];
