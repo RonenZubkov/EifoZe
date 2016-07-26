c1c93a236547accf2d625aa54dd44b7febedef70
Ronen Zubkov <ronenownz@gmail.com>
2016-07-26 19:58:14 +0300

    Map

---
+++ app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+
+
+import {MonsterService} from './monster/monster.service';
+import {ChatRoomService} from './chat/chat-room.service';
+import {LayerService} from './layer/layer.service';
+import {MapComponent} from "./shared/map-component/map.component";
+
+// import * as io from 'socket.io-client';
+
+
+@Component({
+  selector: 'my-app',
+  moduleId: module.id,
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  providers: [MonsterService, ChatRoomService, LayerService, ToastsManager, {provide: 'io', useValue: io}]
+})
+export class AppComponent {
+    title = 'google map\'s app';
+}
---
+++ app/main.ts
@@ -0,0 +1,22 @@
+import { provide } from '@angular/core';
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { provideForms } from '@angular/forms';
+import { HTTP_PROVIDERS } from '@angular/http';
+import { ROUTER_PROVIDERS } from './app.routes';
+
+import { AppComponent } from './app.component';
+
+import {MapsAPILoader, NoOpMapsAPILoader, MouseEvent, GOOGLE_MAPS_PROVIDERS, GOOGLE_MAPS_DIRECTIVES} from 'angular2-google-maps/core';
+
+
+
+
+bootstrap(AppComponent, [
+  provideForms(),
+  ROUTER_PROVIDERS,
+  HTTP_PROVIDERS,
+  GOOGLE_MAPS_PROVIDERS,
+  provide(LocationStrategy, { useClass: PathLocationStrategy })
+]);
+
