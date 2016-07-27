8a0689cdfc40272fa4f192c3c97ef845079855b1
zafrirdotan <zafrir.dotan@gmail.com>
2016-07-27 18:42:17 +0300

    no big chaing

--- app/shared/editable-items/editable-items.component.ts
+++ app/shared/editable-items/editable-items.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators, REACTIVE_FORM_DIRECTIVES, FormControl} from '@angular/forms';
 @Component({
   moduleId: module.id,
   styleUrls: [],
   selector: 'editable-items',
+  directives: [REACTIVE_FORM_DIRECTIVES],
   template: `
     <section>
+      <form [formGroup]="frmItem" (submit)="save()" >
+        <div class="form-group">
+          <label>Name:</label>
+          <input type="text" class="form-control" formControlName="name">
+        </div>
+
+        <div class="form-group">
+          <label>Power:</label>
+          <input type="number" class="form-control" formControlName="lat">
+        </div>
+
+        <div class="form-group">
+        <label>Price:</label>
+        <input type="number" class="form-control" formControlName="lng">
+        </div>
+
+        <button type="submit" [disabled]="!frmItem.valid" class="btn btn-default">Submit</button>
+      </form>
+
       <h2> Deletable Items</h2>
 
     
@@ -31,11 +51,12 @@ export class EditableItemsComponent  {
 @Output() private add = new EventEmitter();
 @Input() private items; 
 
+ private frmItem: FormGroup;
   
-  
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.prepareForm();
     }
 
   removeItem( item){
@@ -44,5 +65,16 @@ export class EditableItemsComponent  {
       
       this.remove.emit(item)
     }
- 
+   
+   prepareForm() {
+     this.frmItem = this.formBuilder.group({
+      name: ['',
+              Validators.compose([Validators.required,
+                                  Validators.minLength(3),
+                                  Validators.maxLength(100)])],
+      lat:  [0,Validators.required],
+      lng:  [0,Validators.required]
+      
+    });
+  }
 }
